846bdbd8e921399dbe6f372730b7e701222fc450
Nayara Ferreira <ncy.fers@gmail.com>
2020-07-20 12:18:32 -0300

    adicionando estilo global para o app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import './App.css';
+import GlobalStyle from './styles/global';
 
-function App() {
-  return (
-    <h1>Go Barber</h1>
-  );
-}
+const App: React.FC = () => (
+  <>
+    <h1>GoBarber Web</h1>
+    <GlobalStyle />
+  </>
+);
 
 export default App;
