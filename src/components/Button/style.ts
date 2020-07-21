92313da5c5755e95d00ea889b114f8aac6830ba5
Nayara Ferreira <ncy.fers@gmail.com>
2020-07-21 17:45:40 -0300

    adicionado estilização para o componente button'

--- src/components/Button/style.ts
+++ src/components/Button/style.ts
@@ -1,3 +1,19 @@
 import styled from 'styled-components';
+import { shade } from 'polished';
 
-export const Container = styled.button``;
+export const Container = styled.button`
+  background: #ff9000;
+  height: 56px;
+  color: #312e38;
+  border-radius: 10px;
+  border: 0;
+  padding: 0 16px;
+  width: 100%;
+  font-weight: 700;
+  margin-top: 16px;
+  transition: background-color 0.2s;
+
+  &:hover {
+    background: ${shade(0.2, '#ff9000')};
+  }
+`;
